50e38fb0ead85ef2298ff22e2bcbb622870e6d49
Mithun Bobade <mithun_bobade@linfox.com>
2024-01-17 12:46:35 +0530

    formatting done

--- Labfiles/01-analyze-text/C-Sharp/text-analysis/Program.cs
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/Program.cs
@@ -26,7 +26,7 @@ namespace text_analysis
 
 
                 // Analyze each text file in the reviews folder
-                var folderPath = Path.GetFullPath("./reviews");  
+                var folderPath = Path.GetFullPath("./reviews");
                 DirectoryInfo folder = new DirectoryInfo(folderPath);
                 foreach (var file in folder.GetFiles("*.txt"))
                 {
@@ -37,56 +37,56 @@ namespace text_analysis
                     sr.Close();
                     Console.WriteLine("\n" + text);
 
-                   // Get language
-                  // Create client using endpoint and key
-AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
-Uri endpoint = new Uri(aiSvcEndpoint);
-TextAnalyticsClient aiClient = new TextAnalyticsClient(endpoint, credentials);
+                    // Get language
+                    // Create client using endpoint and key
+                    AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+                    Uri endpoint = new Uri(aiSvcEndpoint);
+                    TextAnalyticsClient aiClient = new TextAnalyticsClient(endpoint, credentials);
 
-// Get language
-DetectedLanguage detectedLanguage = aiClient.DetectLanguage(text);
-Console.WriteLine($"\nLanguage: {detectedLanguage.Name}");
+                    // Get language
+                    DetectedLanguage detectedLanguage = aiClient.DetectLanguage(text);
+                    Console.WriteLine($"\nLanguage: {detectedLanguage.Name}");
 
                     // Get sentiment
                     // Get sentiment
-DocumentSentiment sentimentAnalysis = aiClient.AnalyzeSentiment(text);
-Console.WriteLine($"\nSentiment: {sentimentAnalysis.Sentiment}");
+                    DocumentSentiment sentimentAnalysis = aiClient.AnalyzeSentiment(text);
+                    Console.WriteLine($"\nSentiment: {sentimentAnalysis.Sentiment}");
 
                     // Get key phrases
-// Get key phrases
-KeyPhraseCollection phrases = aiClient.ExtractKeyPhrases(text);
-if (phrases.Count > 0)
-{
-    Console.WriteLine("\nKey Phrases:");
-    foreach(string phrase in phrases)
-    {
-        Console.WriteLine($"\t{phrase}");
-    }
-}
+                    // Get key phrases
+                    KeyPhraseCollection phrases = aiClient.ExtractKeyPhrases(text);
+                    if (phrases.Count > 0)
+                    {
+                        Console.WriteLine("\nKey Phrases:");
+                        foreach (string phrase in phrases)
+                        {
+                            Console.WriteLine($"\t{phrase}");
+                        }
+                    }
 
                     // Get entities
-// Get entities
-CategorizedEntityCollection entities = aiClient.RecognizeEntities(text);
-if (entities.Count > 0)
-{
-    Console.WriteLine("\nEntities:");
-    foreach(CategorizedEntity entity in entities)
-    {
-        Console.WriteLine($"\t{entity.Text} ({entity.Category})");
-    }
-}
+                    // Get entities
+                    CategorizedEntityCollection entities = aiClient.RecognizeEntities(text);
+                    if (entities.Count > 0)
+                    {
+                        Console.WriteLine("\nEntities:");
+                        foreach (CategorizedEntity entity in entities)
+                        {
+                            Console.WriteLine($"\t{entity.Text} ({entity.Category})");
+                        }
+                    }
 
                     // Get linked entities
-// Get linked entities
-LinkedEntityCollection linkedEntities = aiClient.RecognizeLinkedEntities(text);
-if (linkedEntities.Count > 0)
-{
-    Console.WriteLine("\nLinks:");
-    foreach(LinkedEntity linkedEntity in linkedEntities)
-    {
-        Console.WriteLine($"\t{linkedEntity.Name} ({linkedEntity.Url})");
-    }
-}
+                    // Get linked entities
+                    LinkedEntityCollection linkedEntities = aiClient.RecognizeLinkedEntities(text);
+                    if (linkedEntities.Count > 0)
+                    {
+                        Console.WriteLine("\nLinks:");
+                        foreach (LinkedEntity linkedEntity in linkedEntities)
+                        {
+                            Console.WriteLine($"\t{linkedEntity.Name} ({linkedEntity.Url})");
+                        }
+                    }
 
                 }
             }
